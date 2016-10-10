753bdd69edfa11594e3c1f3f96949bb03d38bf06
yoshimura123 <yoshim3@hawaii.edu>
2016-10-09 18:57:47 -1000

    Task 3: Tasklist form

--- app/tasklist.js
+++ app/tasklist.js
@@ -6,6 +6,24 @@ if (Meteor.isClient) {
       return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
+
+  Template.tasks.events({
+    "submit .add-task": function(event) {
+      let name = event.target.name.value;
+      Tasks.insert({
+        name: name, createdAt: new Date()
+      });
+      event.target.name.value = '';
+      return false;
+    },
+
+    "click .delete-task": function(event) {
+      if (confirm('Delete Task?')) {
+        Tasks.remove(this._id);
+      }
+      return false;
+    }
+  })
 }
 
 if (Meteor.isServer) {
