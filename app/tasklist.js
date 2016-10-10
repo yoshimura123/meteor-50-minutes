2f9491ab6d755a49112f8be4f007fe590e1618a4
yoshimura123 <yoshim3@hawaii.edu>
2016-10-09 19:30:52 -1000

    Task 5: subscriptions

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,6 +1,8 @@
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
+  Meteor.subscribe('tasks');
+
   Template.tasks.helpers({
     tasks: function() {
       return Tasks.find({}, {sort: {createdAt: -1}});
@@ -25,7 +27,9 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-
+  Meteor.publish('tasks', function() {
+    return Tasks.find({userId: this.userId});
+  })
 }
 
 Meteor.methods({
